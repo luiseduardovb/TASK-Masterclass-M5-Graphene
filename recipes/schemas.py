23eabd54631597266b7e5ce24b6a03ef2ecb38eb
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-05-26 10:36:18 +0300

    Add mutation

--- recipes/schemas.py
+++ recipes/schemas.py
@@ -1,3 +1,10 @@
+
+import graphene
+
+from food.schemas import FoodMutation, FoodQuery
+from food.models import Recipe
+
+
 class Query(FoodQuery,graphene.ObjectType):
     greet = graphene.String(name=graphene.String())
 
@@ -5,3 +12,9 @@ class Query(FoodQuery,graphene.ObjectType):
         return f'Hello {name}'
 
 
+
+
+class Mutation(FoodMutation, graphene.ObjectType):
+    pass
+
+SCHEMA = graphene.Schema(query=Query, mutation=Mutation)
