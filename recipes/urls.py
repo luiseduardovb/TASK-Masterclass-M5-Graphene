19445f80ec2a3de2901d6fc7cc5b82d220b5b8ba
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-05-26 10:32:14 +0300

    Add url for graphene django

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from graphene_django.views import GraphQLView
+
+from recipes.schemas import SCHEMA
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql="settings.DEBUG", schema=SCHEMA))),
 ]
